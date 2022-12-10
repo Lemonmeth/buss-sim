1a86a2d35e8310c8212f87a3bce45fc8855dcaab
diosmio <diosmio@dm.dm>
2022-12-10 23:18:37 +0100

    lagt till overlfow-metod för möjlighet att använda input-parameter

--- SafeInput.cs
+++ SafeInput.cs
@@ -1,9 +1,14 @@
 namespace Bussen;
 
+// Class för säker inmatning och felhantering
 public class SafeInput
 {
-	static public string Strings(string text)
+	static public string Strings() // Metod för säker inmatning av strings
 	{
+		Console.CursorVisible = true;
+		string text = Console.ReadLine();
+
+		// Kontrollera om text innehåller null eller whitepace
 		if (String.IsNullOrWhiteSpace(text) == false)
 		{
 			return text;
@@ -11,12 +16,31 @@ public class SafeInput
 		else
 		{
 			System.Console.WriteLine("Felaktig inmatning, försök igen: ");
-			return Strings(Console.ReadLine());
+			return Strings();
 		}
 	}
 
-	static public int Integers(string strNumber)
+	static public string Strings(string text) // Overflow-metod för säker inmatning av strings, med input-parameter
 	{
+		// Kontrollera om text innehåller null eller whitepace
+		if (String.IsNullOrWhiteSpace(text) == false)
+		{
+			return text;
+		}
+		else
+		{
+			System.Console.WriteLine("Felaktig inmatning, försök igen: ");
+
+			// Testar rekursion, hoppar till första strings-medoten där den ber om ny inmatning.
+			return Strings();
+		}
+	}
+
+	static public int Integers() // Metod för säker inmatning av integers
+	{
+		string strNumber = Console.ReadLine();
+
+		// Om det går att parsa input till helta, skapa ny variabel med heltalet och returnera den
 		if (int.TryParse(strNumber, out int okNumber) == true)
 		{
 			return okNumber;
@@ -24,7 +48,23 @@ public class SafeInput
 		else
 		{
 			System.Console.WriteLine("Felaktig inmatning, försök igen: ");
-			return Integers(Console.ReadLine());
+			return Integers();
+		}
+	}
+
+	static public int Integers(string strNumber) // Overflow-metod för säker inmatning av integers, med input-parameter
+	{
+		// Om det går att parsa input till helta, skapa ny variabel med heltalet och returnera den
+		if (int.TryParse(strNumber, out int okNumber) == true)
+		{
+			return okNumber;
+		}
+		else
+		{
+			System.Console.WriteLine("Felaktig inmatning, försök igen: ");
+
+			// Testar rekursion, hoppar till första integers-medoten där den ber om ny inmatning.
+			return Integers();
 		}
 	}
 }
